fa9b2418a1df68d0be9b84a26129c72e875ecdb4
caimengku <60225005+caimengku@users.noreply.github.com>
2020-06-13 17:05:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -89,9 +89,61 @@ def handle_message(event):
             preview_image_url='https://i.imgur.com/QPJ8A1b.png'
         )
 
-     # 傳送電影推薦
-    elif event.message.text == '影片':
+     # 傳送新聞連結
+    elif event.message.text == '新聞':
         message = TextSendMessage(getNews())
+        
+    elif event.message.text == "Carousel template":
+        Carousel_template = TemplateSendMessage(
+        alt_text='Carousel template',
+        template=CarouselTemplate(
+        columns=[
+            CarouselColumn(
+                thumbnail_image_url='顯示在開頭的大圖片網址',
+                title='this is menu1',
+                text='description1',
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback1',
+                        text='postback text1',
+                        data='action=buy&itemid=1'
+                    ),
+                    MessageTemplateAction(
+                        label='message1',
+                        text='message text1'
+                    ),
+                    URITemplateAction(
+                        label='uri1',
+                        uri='http://example.com/1'
+                    )
+                ]
+            ),
+            CarouselColumn(
+                thumbnail_image_url='顯示在開頭的大圖片網址',
+                title='this is menu2',
+                text='description2',
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback2',
+                        text='postback text2',
+                        data='action=buy&itemid=2'
+                    ),
+                    MessageTemplateAction(
+                        label='message2',
+                        text='message text2'
+                    ),
+                    URITemplateAction(
+                        label='連結2',
+                        uri='http://example.com/2'
+                    )
+                ]
+            )
+        ]
+    )
+    )
+    
+        
+        
         
     # 傳送貼圖
     elif event.message.text == '傳送貼圖':
